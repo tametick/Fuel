10f74953c89a7b3c1f71d20fc83c24ed68f86ed0
Ido Yehieli <ido.yehieli@gmail.com>
2012-02-24 12:21:06 +0100

    weapon stats

--- Source/world/WeaponFactory.hx
+++ Source/world/WeaponFactory.hx
@@ -12,14 +12,29 @@ class WeaponFactory {
 		switch (type) {
 			case UNARMED:	
 				w.range = Registry.rangeShort;
+				w.damage = 1;
+				w.attackSpeed = 4;
+				w.accuracy = 4;
 			case SPEAR:
 				w.range = Registry.rangeShort;
+				w.damage = 6;
+				w.attackSpeed = 2;
+				w.accuracy = 3;
 			case SWORD:
 				w.range = Registry.rangeShort;
+				w.damage = 4;
+				w.attackSpeed = 3;
+				w.accuracy = 4;
 			case BOW:
 				w.range = Registry.rangeLong;
+				w.damage = 2;
+				w.attackSpeed = 3;
+				w.accuracy = 4;
 			case STAFF:
 				w.range = Registry.rangeShort;
+				w.damage = 3;
+				w.attackSpeed = 2;
+				w.accuracy = 5;
 		}
 		
 		var color = 0xffffffff;
