d278dac0f911dcb3e23619f37f0b44a7b8cbf074
ido.yehieli <ido.yehieli@localhost>
2012-03-16 17:59:07 +0100

    wall climbers AI done

--- Source/parts/ClimberAiPart.hx
+++ Source/parts/ClimberAiPart.hx
@@ -1,13 +1,16 @@
 package parts;
-import data.Registry;
+
 import org.flixel.FlxU;
+import data.Registry;
+import utils.Direction;
 import utils.Utils;
 
 class ClimberAiPart extends AiPart{
 	override public function act() {
 		var p = Registry.player.tilePoint;
 		var l = Registry.level;
-		
+		var s = actor.sprite;
+		var d = s.direction;
 		
 		if (actor.isHung) {
 			if (Utils.dist(p.x,actor.tileX)<2 &&
@@ -16,8 +19,23 @@ class ClimberAiPart extends AiPart{
 				actor.isHung = false;
 				actor.sprite.play("idle");
 			}
-		} else {
-		
+		} else if(actor.isOnGround()){
+			if (canAttackPlayer()) {
+				actor.weapon.fire();
+			} else if (canWalkForward()) {
+				var dx = d == W? -1:1;
+				if (Registry.level.isWalkable(actor.tileX + dx, actor.tileY)) {
+					s.startMoving(dx,0);
+				}
+			} else {
+				// turn around
+				s.direction = Utils.reverseDirection(s.direction);
+				if (s.direction == E) {
+					s.faceRight();
+				} else {
+					s.faceLeft();
+				}
+			}
 		}
 	}
 }
