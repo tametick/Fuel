823322a7904976a6d6cc343373ea1109cf9cb5dc
ido.yehieli <ido.yehieli@localhost>
2012-03-16 13:45:55 +0100

    walkers attacking

--- Source/parts/WalkerAiPart.hx
+++ Source/parts/WalkerAiPart.hx
@@ -11,7 +11,9 @@ class WalkerAiPart extends AiPart{
 		var s = actor.sprite;
 		var d = s.direction;
 	
-		if (canWalkForward()) {
+		if (canAttackPlayer()) {
+			actor.weapon.fire();
+		} else if (canWalkForward()) {
 			var dx = d == W? -1:1;
 			if (Registry.level.isWalkable(actor.tileX + dx, actor.tileY)) {
 				s.startMoving(dx,0);
@@ -57,6 +59,22 @@ class WalkerAiPart extends AiPart{
 				}
 			case E:
 				if (l.get(actor.tilePoint.x + 1, actor.tilePoint.y) == 1 && l.get(actor.tilePoint.x + 1, actor.tilePoint.y+1) == 1) {
+					return true;
+				}
+			default:
+		}
+		return false;
+	}
+	
+	function canAttackPlayer():Bool {
+		var l = Registry.level;
+		switch (actor.sprite.direction) {
+			case W:
+				if (Registry.player.tileX==actor.tilePoint.x - 1 && Registry.player.tileY==actor.tilePoint.y) {
+					return true;
+				}
+			case E:
+				if (Registry.player.tileX==actor.tilePoint.x + 1 && Registry.player.tileY==actor.tilePoint.y) {
 					return true;
 				}
 			default:
